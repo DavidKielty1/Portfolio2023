2771860fd757f982385f44ecdbbec3107176b459
David Kielty <davidkielty@live.co.uk>
2023-10-02 12:12:53 +0100

    Changed About Me sectino content

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -138,36 +138,21 @@ export default function AboutSection() {
         id="animeCanvas"
         className="absolute top-0 left-0 w-full h-full z-[-1]"
       ></canvas>
-      <div className="max-w-screen-2xl flex flex-col items-center h-full w-9/12 self-center bg-opacity-70 z-10 position-relative">
+      <div className="max-w-screen-lg flex flex-col items-center h-full w-9/12 self-center bg-opacity-70 z-10 position-relative">
         <h2 className="text-center font-raleway text-5xl font-bold py-10 md:py-16 xl:pt-48 ">
           About Me
         </h2>
         <p className="font-montserrat text-2xl text-center md:text-justify">
-          I&apos;m a dedicated software developer seeking opportunities at
-          reputable companies. My passion lies in working with the latest tech
-          on diverse projects and constantly expanding my skills through
-          hands-on work and staying updated with modern methodologies.
+          Full-stack developer skilled in Javascript(React-Node),
+          C#(.NET-WebAPI) and integrating them seamlessly. Proficient in SQL
+          varieties, cloud deployment, and ensuring front-end safety using tools
+          like TypeScript, Zod, tRPC,ESLint and Prisma ORM. Experienced with
+          testing CI/CD pipelines using GitHub Actions and Docker, and familiar
+          with CSS frameworks like Tailwind, MaterialUI and CSS modules.
         </p>
         <br />
         <p className="hidden md:block font-montserrat text-2xl text-center md:text-justify">
-          I ensure robust development with TypeScript, utilizing Zod, tRPC, and
-          type-safe ORMs such as Prisma while maintaining strict linting
-          practices. My experience includes deploying to various platforms and
-          working with databases such as PostgreSQL, Firebase, and MongoDB. I
-          have also dabbled with setting up testing CI/CD pipelines using GitHub
-          Actions, yaml and Docker.
-        </p>
-        <br />
-        <p className="hidden md:block font-montserrat text-2xl text-center md:text-justify">
-          My CSS skills encompass CSS modules, SCSS, SASS, styled components,
-          and popular frameworks like Tailwind, DaisyUI, and BootstrapUI. In the
-          world of React, I&apos;m well-versed in hooks, context API, fetch API,
-          react-query, and global state management (with Redux).
-        </p>
-        <br />
-        <p className="font-montserrat text-2xl text-center md:text-justify">
-          I am looking forward to collaborating with your established team in an
-          agile environment and learning from your knowledgable expertise.
+          Get in touch. I am excited to collaborate on impactful solutions!
         </p>
       </div>
     </section>
---
+++ src/app/components/AboutSectionHelper.md
@@ -0,0 +1,32 @@
+<div className="max-w-screen-2xl flex flex-col items-center h-full w-9/12 self-center bg-opacity-70 z-10 position-relative">
+<h2 className="text-center font-raleway text-5xl font-bold py-10 md:py-16 xl:pt-48 ">
+  About Me
+</h2>
+<p className="font-montserrat text-2xl text-center md:text-justify">
+  I&apos;m a dedicated software developer seeking opportunities at
+  reputable companies. My passion lies in working with the latest tech
+  on diverse projects and constantly expanding my skills through
+  hands-on work and staying updated with modern methodologies.
+</p>
+<br />
+<p className="hidden md:block font-montserrat text-2xl text-center md:text-justify">
+  I ensure robust development with TypeScript, utilizing Zod, tRPC, and
+  type-safe ORMs such as Prisma while maintaining strict linting
+  practices. My experience includes deploying to various platforms and
+  working with databases such as PostgreSQL, Firebase, and MongoDB. I
+  have also dabbled with setting up testing CI/CD pipelines using GitHub
+  Actions, yaml and Docker.
+</p>
+<br />
+<p className="hidden md:block font-montserrat text-2xl text-center md:text-justify">
+  My CSS skills encompass CSS modules, SCSS, SASS, styled components,
+  and popular frameworks like Tailwind, DaisyUI, and BootstrapUI. In the
+  world of React, I&apos;m well-versed in hooks, context API, fetch API,
+  react-query, and global state management (with Redux).
+</p>
+<br />
+<p className="font-montserrat text-2xl text-center md:text-justify">
+  I am looking forward to collaborating with your established team in an
+  agile environment and learning from your knowledgable expertise.
+</p>
+</div>
